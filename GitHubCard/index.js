b44e6aff9ff7529aa08ae4279467679e6297d71a
Andrew Clark <andrewclark@Andrews-MacBook-Pro-2.local>
2022-06-09 16:36:13 -0500

    all cards displaying on webpage

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,18 @@
+import axios from "axios";
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+const cards = document.querySelector('.cards');
+
+function getUserCards(username) {
+  axios.get(`https://api.github.com/users/${username}`)
+  .then(res => {
+    cards.appendChild(cardMaker(res.data))
+  })
+  .catch(err => console.error(err));
+}
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -28,8 +38,54 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = ['harrisclark', 'tetondan', 'dustinmyers', 'justsml', 'luishrd', 'bigknell'];
 
+followersArray.forEach(username => {
+  getUserCards(username);
+})
+
+function cardMaker(userData) {
+  const card = document.createElement('div');
+  const cardImg = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const nameHeader = document.createElement('h3');
+  const usernameP = document.createElement('p');
+  const locationP = document.createElement('p');
+  const profileP = document.createElement('p');
+  const profileLink = document.createElement('a');
+  const followersP = document.createElement('p');
+  const followingP = document.createElement('p');
+  const bioP = document.createElement('p');
+
+  card.classList.add('card');
+  cardInfo.classList.add('card-info');
+  nameHeader.classList.add('name');
+  usernameP.classList.add('username');
+
+  cardImg.src = userData.avatar_url;
+  nameHeader.textContent = userData.name;
+  usernameP.textContent = userData.login;
+  locationP.textContent = `Location: ${userData.location}`;
+  profileP.textContent = 'Profile: ';
+  profileLink.textContent = userData.html_url;
+  profileLink.href = userData.html_url;
+  followersP.textContent = `Followers: ${userData.followers}`;
+  followingP.textContent = `Following: ${userData.following}`;
+  bioP.textContent = `Bio: ${userData.bio}`;
+
+  card.appendChild(cardImg);
+  card.appendChild(cardInfo);
+  cardInfo.appendChild(nameHeader);
+  cardInfo.appendChild(usernameP);
+  cardInfo.appendChild(locationP);
+  cardInfo.appendChild(profileP);
+  profileP.appendChild(profileLink);
+  cardInfo.appendChild(followersP);
+  cardInfo.appendChild(followingP);
+  cardInfo.appendChild(bioP);
+
+  return card;
+}
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
